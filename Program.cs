300a316ffb67b8204a6a8f217de5ba001d75e7b7
Poulshravani <poulshravani2@gmail.com>
2025-05-07 21:09:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Avg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 10, 20, 30, 40, 50, };
+            double sum = 0;
+
+            foreach (int num in number)
+            {
+                sum += num;
+            }
+            double average = sum / number.Length;
+            Console.WriteLine(average);
+        }
+    }
+}
